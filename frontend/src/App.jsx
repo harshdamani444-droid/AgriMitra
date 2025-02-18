dbb93113af4efb88840c09b53b78312071b3f31b
Nisharg Soni <ncsoni9898@gmail.com>
2025-02-18 20:16:28 +0530

    Issues Solved

---
+++ frontend/src/App.jsx
@@ -0,0 +1,31 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import Shop from "./pages/Shop";
+import News from "./pages/News";
+import Weather from "./pages/Weather";
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
+function App() {
+  return (
+    <Router>
+      <div className="min-h-screen flex flex-col">
+        <Navbar />
+        <div className="flex-grow">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/shop" element={<Shop />} />
+            <Route path="/news" element={<News />} />
+            <Route path="/weather" element={<Weather />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<SignUp />} />
+          </Routes>
+        </div>
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
